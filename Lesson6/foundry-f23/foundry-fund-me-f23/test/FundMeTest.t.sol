aac39b2417baa68e9b347af63c00c5a2cc3e94a0
bxpana <ba@matterlabs.dev>
2024-08-16 11:16:33 -0700

    deployment scripts

--- Lesson6/foundry-f23/foundry-fund-me-f23/test/FundMeTest.t.sol
+++ Lesson6/foundry-f23/foundry-fund-me-f23/test/FundMeTest.t.sol
@@ -9,10 +9,18 @@ contract FundMeTest is Test {
     FundMe fundMe; // declares state variable of type FundMe so that it's accessible by all functions within the FundMeTest contract
 
     function setUp() external {
+        // us -> calling to FundMeTest -> deploys FundMe so owner of FundMe is FundMeTest
         fundMe = new FundMe();
     }
 
     function testMinimumDollarIsFive() public {
         assertEq(fundMe.MINIMUM_USD(), 5e18); // to call this we need access to "fundMe"
+    }
+
+    function testOwnerIsMsgSender() public {
+        console.log(fundMe.i_owner());
+        console.log(msg.sender);
+        console.log(address(this));
+        assertEq(fundMe.i_owner(), address(this));
     }
 }
